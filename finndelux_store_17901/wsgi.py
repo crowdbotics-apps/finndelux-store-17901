de6818849333cfd32edf88efdd159686f3967b7c
Crowdbotics <team@crowdbotics.com>
2020-06-06 23:05:51 +0000

    Initial commit.

---
+++ finndelux_store_17901/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for finndelux_store_17901 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finndelux_store_17901.settings')
+
+application = get_wsgi_application()
